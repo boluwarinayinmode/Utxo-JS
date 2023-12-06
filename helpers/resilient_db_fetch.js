13c171cf3ac43ac6ccb12781d1bd12a818d0bd27
Viral <viralrbhadeshiya@gmail.com>
2023-12-06 13:25:24 -0800

    real time data read from resillient db done

--- helpers/resilient_db_fetch.js
+++ helpers/resilient_db_fetch.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
 const fs = require('fs');
+const { Module } = require('module');
 
 // Don't add forward slash at the end
 const resilliendb_url = 'https://4a49-168-150-28-100.ngrok-free.app';
 
 const transform_resillient_data = async (resilientData) => {
     try {
-        const fs_data = fs.readFileSync('./database/raw_data.json');
-        const input = fs_data ? JSON.parse(fs_data) : [];
-        console.log(input);
+        const input = resilientData
 
         const transactions = input.flatMap((entry) => {
             if (entry.operation === "TRANSFER") {
@@ -42,33 +41,34 @@ const transform_resillient_data = async (resilientData) => {
         console.warn('Error transforming data, retrying in 5 seconds...', new Date(), e);
     }
 }
-(async() => {
-    await transform_resillient_data();
-} )()
 
-// export const transactionPolling = async () => {
-//     try {
-//         let shouldContinue;
-//         while (shouldContinue) {
-//             const response_resilient = await axios({
-//                     method: 'get',
-//                     url: `${resilliendb_url}/v1/transactions`
-//                 });
+const transactionPolling = async () => {
+    try {
+        let shouldContinue = true;
+        while (shouldContinue) {
+            console.log('Polling...');
+            const response_resilient = await axios({
+                        method: 'get',
+                        url: `${resilliendb_url}/v1/transactions`
+                    });
+                
+            if(response_resilient.data) {
+                // Clear raw-data.json
+                fs.writeFileSync('./database/raw_data.json', '');
+                // Write new data into raw-data.json
+                fs.writeFileSync('./database/raw_data.json', JSON.stringify(response_resilient.data));
+            
+                // Once the data is written, we can start transforming data
+                await transform_resillient_data(response_resilient.data);
+            }
+                    
+            console.log('Polling Done');
+            // Sleep for 30 seconds
+            await new Promise(resolve => setTimeout(resolve, 30000));
+        }
+    } catch (e) {
+            console.warn('Error fetching transactions, retrying in 5 seconds...', new Date());
+    }
+};
 
-//             if(response_resilient.data) {
-//                 // Clear raw-data.json
-//                 fs.writeFileSync('./database/raw_data.json', '');
-//                 // Write new data into raw-data.json
-//                 fs.writeFileSync('./database/raw_data.json', JSON.stringify(response_resilient.data));
-
-//                 // Once the data is written, we can start transforming data
-//                 await transform_resillient_data(response_resilient.data);
-//             }
-
-//             // Sleep for 30 seconds
-//             await new Promise(resolve => setTimeout(resolve, 30000));
-//         }
-//     } catch (e) {
-//             console.warn('Error fetching transactions, retrying in 5 seconds...', new Date());
-//     }
-// };
+module.exports = {transactionPolling};
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require('cors')
-const transactionPolling = require("./helpers/resilient_db_fetch");
+const {transactionPolling} = require("./helpers/resilient_db_fetch");
 
 const v1_routes = require("./routes/v1_router");
 
 const app = express();
 
 app.use(cors());
-// app.use(transactionPolling());
 
 const PORT = 5501;
 
@@ -15,4 +14,5 @@ app.use(`/api/v1`, v1_routes);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
+    // transactionPolling();
 })
