6bdbbadce559114a661ecf37581fc94985b30146
Viral <viralrbhadeshiya@gmail.com>
2023-12-06 13:16:17 -0800

    added data for testing purpose

---
+++ helpers/resilient_db_fetch.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+const fs = require('fs');
+
+// Don't add forward slash at the end
+const resilliendb_url = 'https://4a49-168-150-28-100.ngrok-free.app';
+
+const transform_resillient_data = async (resilientData) => {
+    try {
+        const fs_data = fs.readFileSync('./database/raw_data.json');
+        const input = fs_data ? JSON.parse(fs_data) : [];
+        console.log(input);
+
+        const transactions = input.flatMap((entry) => {
+            if (entry.operation === "TRANSFER") {
+                const from = entry.inputs[0].owners_before[0];
+                const to = entry.outputs[0].public_keys[0];
+                const amount = parseInt(entry.outputs[0].amount);
+
+                // Extract timestamp from metadata (if available)
+                const timestamp =
+                    entry.metadata && entry.metadata.timestamp
+                        ? entry.metadata.timestamp
+                        : null;
+
+                return [
+                    {
+                        from,
+                        to,
+                        amount,
+                        timestamp,
+                    },
+                ];
+            } else {
+                return [];
+            }
+        });
+
+        // Write new data into transformed-data.json
+        fs.writeFileSync('./database/transformed_data.json', '');
+        fs.writeFileSync('./database/transformed_data.json', JSON.stringify(transactions));
+    } catch (e) {
+        console.warn('Error transforming data, retrying in 5 seconds...', new Date(), e);
+    }
+}
+(async() => {
+    await transform_resillient_data();
+} )()
+
+// export const transactionPolling = async () => {
+//     try {
+//         let shouldContinue;
+//         while (shouldContinue) {
+//             const response_resilient = await axios({
+//                     method: 'get',
+//                     url: `${resilliendb_url}/v1/transactions`
+//                 });
+
+//             if(response_resilient.data) {
+//                 // Clear raw-data.json
+//                 fs.writeFileSync('./database/raw_data.json', '');
+//                 // Write new data into raw-data.json
+//                 fs.writeFileSync('./database/raw_data.json', JSON.stringify(response_resilient.data));
+
+//                 // Once the data is written, we can start transforming data
+//                 await transform_resillient_data(response_resilient.data);
+//             }
+
+//             // Sleep for 30 seconds
+//             await new Promise(resolve => setTimeout(resolve, 30000));
+//         }
+//     } catch (e) {
+//             console.warn('Error fetching transactions, retrying in 5 seconds...', new Date());
+//     }
+// };
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const  cors = require('cors')
+const cors = require('cors')
+const transactionPolling = require("./helpers/resilient_db_fetch");
 
 const v1_routes = require("./routes/v1_router");
 
 const app = express();
 
 app.use(cors());
+// app.use(transactionPolling());
 
 const PORT = 5501;
 
